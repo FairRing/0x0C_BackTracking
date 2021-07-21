5ca0438e878152e22d4eb585fa7ad0dfdfc8e26f
FairRing <kjh122333@gmail.com>
2021-07-21 20:36:17 +0900

    done.

---
+++ Q6BaaarkingDog15655_N&M6/README.md
@@ -0,0 +1,3 @@
+# N과 M (6)
+
+- 출처 : [https://www.acmicpc.net/problem/15655](https://www.acmicpc.net/problem/15655)
---
+++ Q6BaaarkingDog15655_N&M6/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N, M;
+    static int[] elements, output;
+    static boolean[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        // init from console input...
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = reader.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        // set arrays length...
+        elements = new int[N];
+        isVisited = new boolean[N];
+        output = new int[N];
+        input = reader.readLine().split(" ");
+        for (int i = 0; i < N; i++) elements[i] = Integer.parseInt(input[i]);
+        Arrays.sort(elements);
+        // start backTracking...
+        backTracking(elements[0], 0);
+    }
+
+    private static void backTracking(int element, int location) {
+        if (location == M) { // base condition
+            for (int i = 0; i < M; i++) System.out.print(output[i] + " ");
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            if (!isVisited[i] && element <= elements[i]) {
+                isVisited[i] = true;
+                output[location] = elements[i];
+                backTracking(elements[i], location+1);
+                isVisited[i] = false;
+            }
+        }
+    } // end of backTracking.
+}
