6870e856ae9af3b79b6716d55578494e5a99aac1
FairRing <kjh122333@gmail.com>
2021-08-05 00:36:40 +0900

    done..

--- Q18BaaarkingDog1941FamousSevenPrincesses/src/Main.java
+++ Q18BaaarkingDog1941FamousSevenPrincesses/src/Main.java
@@ -1,100 +1,98 @@
 import java.io.*;
 import java.util.*;
 
-class XY{
-    int x, y;
-    XY(int x, int y){
-        this.x=x;
-        this.y=y;
-    }
-}
-
 public class Main {
-    static int MAX=5, ans=0;
-    static boolean visited[] = new boolean[MAX*MAX];
-    static char map[][] = new char[MAX][MAX];
-    static int aroundX[] = {0,0,1,-1}, aroundY[] = {1,-1,0,0};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));    
-        String input;
-
-        for(int i=0; i<MAX; i++) {
-            input = reader.readLine();
-            for(int j=0; j<MAX; j++) map[i][j]=input.charAt(j);
-        }
-        
-        DFS(0,0,0);
-        System.out.println(ans);
-    }
-    public static void DFS(int start, int team, int n) {
-        if(team==7) {
-            if(isPossible(n)) ans++;
-            return ;
-        }
-        
-        for(int i=start; i<25; i++) {
-            if(!visited[n|(1<<i)]) {
-                visited[n|(1<<i)] = true;
-                DFS(i,team+1,n|(1<<i));
-                visited[n|(1<<i)] = false;
-            }
+    static class Point{
+        int y, x;
+        Point(int y, int x){
+            this.y = y; this.x = x;
         }
     }
 
-    public static boolean isPossible(int n) {
-        ArrayList<Integer>list = new ArrayList<>();
-        for(int i=0; i<25; i++) {
-            if((n & (1<<i))!=0) {
-                list.add(i);
-            }
+    static char[][] seat = new char[5][5];
+    static Map<Integer, Point> map = new HashMap<>();                 
+    static List<Integer> list = new ArrayList<>();
+    static int[] dy = {1, -1, 0 ,0}, dx = {0, 0, 1, -1};
+    static int count, S, Y;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        comb(0);
+        System.out.println(count);
+    }
+
+    public static void comb(int index) {
+        if(Y==4) return;
+
+        if(list.size()==7) {
+
+            count = (bfs()==true ? count+1 : count);
+            return;
         }
 
-        // CHECK CONNECTED
-        boolean map_visited[][] = new boolean[5][5];
-        for(int i=0; i<list.size(); i++) {
-            int r = list.get(i)/5;
-            int c = list.get(i)%5;
-            map_visited[r][c] =true;
+        for(int i = index; i < 25; i++) {
+            Point next = map.get(i);
+
+
+            S = seat[next.y][next.x]=='S' ? S+1 : S;
+            Y = seat[next.y][next.x]=='Y' ? Y+1 : Y;
+            list.add(i);
+
+            comb(i+1);
+
+
+            list.remove((Integer)i);
+            S = seat[next.y][next.x]=='S' ? S-1 : S;
+            Y = seat[next.y][next.x]=='Y' ? Y-1 : Y;
         }
-        
-        Queue<XY>q = new LinkedList<>();
-        q.add(new XY(list.get(0)/5,list.get(0)%5));
-        map_visited[list.get(0)/5][list.get(0)%5]=false;
-        
-        int bfs_cnt =0;
+    }
+
+    public static boolean bfs() {
+        Queue<Point> q = new LinkedList<>();
+        boolean[][] v = new boolean[5][5];
+        boolean[][] m = new boolean[5][5];
+        int cnt = 1;
+
+        for(int i : list) {
+            Point c = map.get(i);
+            m[c.y][c.x] = true; 
+        }
+
+        q.offer(map.get(list.get(0)));
+        v[map.get(list.get(0)).y][map.get(list.get(0)).x] = true;
+
         while(!q.isEmpty()) {
-            XY a= q.poll();
-            bfs_cnt++;
-            for(int i=0; i<4; i++) {
-                int nx = a.x+aroundX[i];
-                int ny = a.y+aroundY[i];
-                if(isRange(nx,ny) && map_visited[nx][ny]==true) {
-                    q.add(new XY(nx,ny));
-                    map_visited[nx][ny]=false;
-                }
-            }
-        }
+            Point c = q.poll();
 
-        // FAIL CASE :: NOT CONNECTED ALL
-        if(bfs_cnt!=7) return false;
-        
-        // FALE CASE :: 이다솜 > 4
-        int s =0;
-        for(int i=0; i<list.size(); i++) {
-            int tmp_r = list.get(i)/5;
-            int tmp_c = list.get(i)%5;
-            if(map[tmp_r][tmp_c]=='S') {
-                s++;
+            for(int i = 0; i < 4; i++) {
+                Point next = new Point(c.y+dy[i], c.x+dx[i]);
+                if(!isIn(next) || v[next.y][next.x] || m[next.y][next.x]==false) continue;
+
+                v[next.y][next.x] = true; 
+                cnt++;
+                q.offer(next);
             }
         }
-        if(s<4)  return false;
-        return true;
+        return cnt==7 ? true : false;
     }
-    public static boolean isRange(int x, int y) {
-        
-        if(x>=0 && y>=0 && x<5 && y<5) return true;
 
-        return false;
+    public static boolean isIn(Point c) {
+        if(0 <= c.y && c.y < 5 && 0 <= c.x && c.x < 5) return true;
+        else return false;
+    }
+
+    public static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int i = 0; i < 5; i++) {
+            seat[i] = br.readLine().toCharArray();
+        }
+
+        int index = 0;
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                map.put(index++, new Point(i, j));
+            }
+        }
     }
 }
